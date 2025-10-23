1bb38f89e59482da2b565834ef72c25e5f4a51e9
ead <ead@academico.local>
2025-10-23 11:47:42 -0300

    32102025

--- .venv/Scripts/activate.bat
+++ .venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set "VIRTUAL_ENV=C:\Users\ead\Documents\projeto_CRUD\.venv"
+set "VIRTUAL_ENV=C:\Users\ead\Documents\Projeto-CRUD\.venv"
 
 if not defined PROMPT set PROMPT=$P$G
 
